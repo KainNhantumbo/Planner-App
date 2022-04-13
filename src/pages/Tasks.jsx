8562e36c9317133bfe398852b6a42347f4f52493
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-13 17:12:20 +0200

    feat: added tasks component items structure.

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import { BiTask } from 'react-icons/bi';
+import { BiCheck, BiCheckbox, BiData, BiStar, BiTask, BiTrash } from 'react-icons/bi';
 import TitleBars from '../components/TitleBars';
 import { TasksContainer } from '../styles/tasks';
 import axios from 'axios';
@@ -12,18 +12,33 @@ const Tasks = () => {
 		try {
 			const url = `http://localhost:4500/api/v1/tasks`;
 			const { data } = await axios({ method: 'get', url: url });
-			setTasksData(() => data);
+			setTasksData(() => data.data);
 		} catch (e) {
 			console.log(e);
 		}
 	};
-	useEffect(() => getTasks(), []);
-
+	useEffect(() => {
+		getTasks();
+	}, []);
+	
 	return (
 		<TasksContainer>
 			<TitleBars title={'Tasks'} icon={<BiTask />} />
 			<section>
-				<div className='task-container'></div>
+				<div className='task-container'>
+					{tasksData.map(({ _id, task, completed }) => {
+						return (
+							<div key={_id}>
+								<BiTask/>
+								<label htmlFor='task'>
+									<span id='task'>{task}</span>
+								</label>
+								<input type="checkbox" />
+								<button><BiTrash/></button>
+							</div>
+						);
+					})}
+				</div>
 			</section>
 		</TasksContainer>
 	);
--- src/styles/tasks.js
+++ src/styles/tasks.js
@@ -18,6 +18,13 @@ export const TasksContainer = styled.div`
 	margin: 0 auto;
 
   .task-container {
-    
+    position: relative;
+		display: flex;
+		flex-direction: column;
+		border-radius: 5px;
+		box-shadow: 0 0 5px rgb(${({ theme }) => theme.shadows});
+		padding: 5px;
+		cursor: pointer;
+		gap: 0.5em;
   }
 `;

4cf2853eb473567dde4f3a13f039a0caf52ea309
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-13 16:40:50 +0200

    feat: added support to fetch contacts from the server.

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,16 +1,29 @@
 import { BiTask } from 'react-icons/bi';
 import TitleBars from '../components/TitleBars';
 import { TasksContainer } from '../styles/tasks';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Tasks = () => {
+	const [tasksData, setTasksData] = useState([]);
+
+	// get all tasks from the server api
+	const getTasks = async () => {
+		try {
+			const url = `http://localhost:4500/api/v1/tasks`;
+			const { data } = await axios({ method: 'get', url: url });
+			setTasksData(() => data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+	useEffect(() => getTasks(), []);
+
 	return (
 		<TasksContainer>
-			<TitleBars title={'Tasks'} icon={<BiTask/>}/>
+			<TitleBars title={'Tasks'} icon={<BiTask />} />
 			<section>
-				<div className='task-container'>
-					
-				</div>
+				<div className='task-container'></div>
 			</section>
 		</TasksContainer>
 	);

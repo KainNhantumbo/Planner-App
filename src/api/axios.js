cd30ff315249fa28dcfce851584b326058dba39a
Kain Nhantumbo <nhantumbok@gmail.com>
2022-09-24 21:17:12 +0200

    dev: added custom axios config.

---
+++ src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4500/api/v1/auth';
+
+const apiClient = axios.create({ baseURL: BASE_URL });
+
+apiClient.defaults.headers.common['Accept'] = 'application/json';
+apiClient.defaults.headers.common['Content-Type'] = 'application/json';
+apiClient.interceptors.response.use(undefined, (err) => {
+	if (err.response?.status == 403) {
+		location.assign('/login');
+	}
+	return Promise.reject(err);
+});
+
+export default apiClient;

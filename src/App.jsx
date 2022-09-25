9fac0b9cf4a742dca315c3b20e9e796d22cf5b14
Kain Nhantumbo <nhantumbok@gmail.com>
2022-09-25 10:35:22 +0200

    refactor: optimized App component code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { GlobalStyles } from './styles/globalstyles';
 import {
 	BiPaperPlane,
 	BiCategory,
@@ -12,37 +9,30 @@ import {
 	BiLogOut,
 } from 'react-icons/bi';
 import AppRoutes from './routes/AppRoutes';
-import { HeaderContainer } from './styles/header';
+import { Link } from 'react-router-dom';
+import { GlobalStyles } from './styles/globalstyles';
 import { ThemeProvider } from 'styled-components';
+import { HeaderContainer } from './styles/header';
+import { useState, useEffect } from 'react';
 import { lightTheme, darkTheme } from './styles/themes';
 
-function App() {
-	// menu bar states management
+export default function App() {
+	// menu toggler
 	const [menuStatus, setMenuStatus] = useState('none');
-
-	// theme setup
-	const [iconMode, setIconMode] = useState({ icon: <BiSun />, name: 'sun' });
-
-	// picks theme configuration from localStorage
-	const themeDataPicker = () => {
-		let mode = JSON.parse(localStorage.getItem('theme'));
-		if (!mode || mode === undefined) {
-			mode = 'sun';
-			localStorage.setItem('theme', JSON.stringify(mode));
-		}
-		return mode;
+	const menuShow = () => {
+		if (menuStatus === 'none') return setMenuStatus(() => 'block');
+		return setMenuStatus(() => 'none');
 	};
-	const themeData = themeDataPicker();
 
-	const [colors, setColors] = useState(() => {
-		if (themeData === 'sun') {
-			return lightTheme;
-		} else {
-			return darkTheme;
-		}
-	});
+	const logoutUser = () => {
+		localStorage.removeItem('token');
+		menuShow();
+	};
+
+	const [iconMode, setIconMode] = useState({ icon: <BiSun />, name: 'sun' });
+	const [colors, setColors] = useState();
+
 	// switches between dark and light themes
-	// and sets the configuration into localStorage
 	const modeSwitcher = () => {
 		if (colors === lightTheme) {
 			setIconMode({ icon: <BiMoon />, name: 'moon' });
@@ -54,76 +44,67 @@ function App() {
 			setColors(() => lightTheme);
 		}
 	};
-	// ===================================== //
-	// menu toggler
-	const menuShow = () => {
-		if (menuStatus === 'none') {
-			return setMenuStatus(() => 'block');
-		}
-		return setMenuStatus(() => 'none');
-	};
-	// ======================================== //
-	// log out user
-	const logoutUser = () => {
-		// removes the token on localstorage
-		localStorage.removeItem('token');
-		menuShow();
-	};
+
+	useEffect(() => {
+		setColors(() => {
+			const themeData = JSON.parse(
+				localStorage.getItem('theme') || `{"theme":"sun"}`
+			);
+			if (themeData === 'sun') return lightTheme;
+			return darkTheme;
+		});
+	}, []);
 
 	return (
-		<>
-			<ThemeProvider theme={colors}>
-				<GlobalStyles />
-				<HeaderContainer>
-					<div className='logoSection'>
-						<BiPaperPlane className='logo' />
-						<span>Planner</span>
-						<button onClick={modeSwitcher}>{iconMode.icon}</button>
-						<button onClick={menuShow}>
-							<BiCategory />
-						</button>
-					</div>
-					<nav
-						className='navbar'
-						style={{ display: menuStatus }}
-						onClick={(e) => {
-							if (e.target.tagName === 'NAV') {
-								menuShow();
-							}
-						}}
-					>
-						<ul>
-							<Link to='/' onClick={menuShow}>
-								<li>
-									<BiSpreadsheet />
-									<span>Tasks</span>
-								</li>
-							</Link>
-							<Link to='/contacts' onClick={menuShow}>
-								<li>
-									<BiPhone />
-									<span>Contacts</span>
-								</li>
-							</Link>
-							<Link to='/settings' onClick={menuShow}>
-								<li>
-									<BiCog />
-									<span>Settings</span>
-								</li>
-							</Link>
-							<Link to='login' onClick={logoutUser}>
-								<li>
-									<BiLogOut />
-									<span>LogOut</span>
-								</li>
-							</Link>
-						</ul>
-					</nav>
-				</HeaderContainer>
-				<AppRoutes />
-			</ThemeProvider>
-		</>
+		<ThemeProvider theme={colors}>
+			<GlobalStyles />
+			<HeaderContainer>
+				<div className='logoSection'>
+					<BiPaperPlane className='logo' />
+					<span>Planner</span>
+					<button onClick={modeSwitcher}>{iconMode.icon}</button>
+					<button onClick={menuShow}>
+						<BiCategory />
+					</button>
+				</div>
+				<nav
+					className='navbar'
+					style={{ display: menuStatus }}
+					onClick={(e) => {
+						if (e.target.tagName === 'NAV') {
+							menuShow();
+						}
+					}}
+				>
+					<ul>
+						<Link to='/' onClick={menuShow}>
+							<li>
+								<BiSpreadsheet />
+								<span>Tasks</span>
+							</li>
+						</Link>
+						<Link to='/contacts' onClick={menuShow}>
+							<li>
+								<BiPhone />
+								<span>Contacts</span>
+							</li>
+						</Link>
+						<Link to='/settings' onClick={menuShow}>
+							<li>
+								<BiCog />
+								<span>Settings</span>
+							</li>
+						</Link>
+						<Link to='login' onClick={logoutUser}>
+							<li>
+								<BiLogOut />
+								<span>LogOut</span>
+							</li>
+						</Link>
+					</ul>
+				</nav>
+			</HeaderContainer>
+			<AppRoutes />
+		</ThemeProvider>
 	);
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
 import './sass/main.css';
 import App from './App';
-
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   	<Router>
+	<StrictMode>
+		<Router>
 			<App />
 		</Router>
-  </React.StrictMode>
-)
+	</StrictMode>
+);
